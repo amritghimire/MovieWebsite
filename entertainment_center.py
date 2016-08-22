b8de3bcbaa67a621e4705cd2ed97127255604965
Amrit Ghimire <iamritghimire@gmail.com>
2016-08-22 15:14:20 +0545

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,15 +4,18 @@ import fresh_tomatoes
 from media import Movie
 
 # Create objects for movie
-toy_story = Movie(movie_title="Toy Story",
-                  youtube_trailer_url="https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = Movie("Toy Story",
+                  "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = Movie("Avatar",
                "http://youtube.com/watch?v=5PSNL1qE6VY")
+
 school_of_rocks = Movie("School of Rock",
                         "https://youtube.com/watch?v=3PsUJFEBC74")
+
 ratatouille = Movie("Ratatouille",
                     "https://youtube.com/watch?v=c3sBBRxDAqk")
+
 midnight_in_paris = Movie("Midnight in paris",
                           "https://youtube.com/watch?v=FAfR8omt-CY")
 
