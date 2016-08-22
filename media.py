c2a438ec500e3836e5dcf56bbb3b562de75dd7fd
Amrit Ghimire <iamritghimire@gmail.com>
2016-08-22 15:09:01 +0545

    Update media.py

--- media.py
+++ media.py
@@ -7,8 +7,11 @@ class Movie:
     """
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
+    # Constructor for Movie class
     def __init__(self, movie_title, youtube_trailer_url=""):
+        """ store the information about movie for the instance
+        Parameter: title of movie and link to trailer of movie on youtube
+        """
         self.title = movie_title
         url = "http://www.omdbapi.com/?t="+movie_title.replace(" ","+")+"&y=&plot=short&r=json"
         response = requests.get(url)
@@ -16,6 +19,3 @@ class Movie:
         self.storyline = r['Plot'].encode('utf-8')
         self.poster_image_url = r['Poster']
         self.trailer_youtube_url = youtube_trailer_url
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
